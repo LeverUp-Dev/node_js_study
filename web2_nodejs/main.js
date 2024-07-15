b010a172a54776193fbcfff54108ffe30012b2f6
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-15 21:49:43 +0900

    url과 모듈 맛보기

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -1,16 +1,19 @@
-var http = require("http");
-var fs = require("fs");
+// 모듈 - C#이나 자바로 치면 패키지 같은 것 using, import 등등 (라이브러리에서 불러오는 기능들)
+var http = require("http"); //http 요구
+var fs = require("fs"); // fs 요구 fs? 아직 모름
+var url = require("url"); //require 요구하다
+
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query; //parse 분석하다
+  console.log(queryData);
+  if (_url == "/") {
+    _url = "/index.html";
   }
-  if (request.url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
-    return;
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
   }
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(fs.readFileSync(queryData));
 });
 app.listen(3000);
