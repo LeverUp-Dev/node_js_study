9bdee61a0b557a6ae82179f10262084b02ac06f6
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-19 09:39:00 +0900

    not found 구현

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -6,37 +6,37 @@ var url = require("url"); //require 요구하다
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query; //parse 분석하다
+  var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
-  console.log(queryData.id);
-  if (_url == "/") {
-    title = "Welcome";
+
+  if (pathname === "/") {
+    fs.readFile(`data/${title}`, "utf-8", function (err, description) {
+      var template = `
+    <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+    `;
+      response.end(template);
+      response.writeHead(200);
+    });
+  } else {
+    // response.writeHead(404);
+    response.end("not found");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
-    var template = `
-  <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-  `;
-    response.end(template);
-  });
 });
 app.listen(3000);
 // 실습 시 복붙은 지양하자 아마도 복붙으로 인해서 오류가 난 것으로 보임
