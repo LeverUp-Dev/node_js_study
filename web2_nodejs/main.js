087cf581bdc93ba675831c9cbc42294a11c1d3a4
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-19 10:03:45 +0900

    실습과 코드 정리

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -1,42 +1,40 @@
-// 모듈 - C#이나 자바로 치면 패키지 같은 것 using, import 등등 (라이브러리에서 불러오는 기능들)
-var http = require("http"); //http 요구
-var fs = require("fs"); // fs(파일 편집기)
-var url = require("url"); //require 요구하다
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  var queryData = url.parse(_url, true).query; //parse 분석하다
+  var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
 
   if (pathname === "/") {
-    fs.readFile(`data/${title}`, "utf-8", function (err, description) {
+    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
       var template = `
-    <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-    `;
-      response.end(template);
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
       response.writeHead(200);
+      response.end(template);
     });
   } else {
-    // response.writeHead(404);
-    response.end("not found");
+    response.writeHead(404);
+    response.end("Not found");
   }
 });
 app.listen(3000);
-// 실습 시 복붙은 지양하자 아마도 복붙으로 인해서 오류가 난 것으로 보임
---
+++ web2_nodejs/syntax/array.js
@@ -0,0 +1,8 @@
+var arr = ['A','B','C','D']; //데이터 삽입
+console.log(arr[1]); //1번 인덱스의 값
+console.log(arr[3]); //3번 인덱스의 값
+arr[2] = 3; //2번 인덱스의 값 변경
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
