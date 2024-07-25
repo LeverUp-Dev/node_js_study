4389c0562a3e5a7619c082b8b9d5d6ebf7a793f1
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-25 22:03:44 +0900

    함수를 이용한 코드 정리

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -2,15 +2,8 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
-
-  if (pathname === "/") {
-    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-      var template = `
+function templateHTML(title, list, body) {
+  return `
         <!doctype html>
         <html>
         <head>
@@ -24,14 +17,59 @@ var app = http.createServer(function (request, response) {
             <li><a href="/?id=CSS">CSS</a></li>
             <li><a href="/?id=JavaScript">JavaScript</a></li>
           </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
+          ${list}
+          ${body}
         </body>
         </html>
         `;
-      response.writeHead(200);
-      response.end(template);
-    });
+}
+function templateList(filelist) {
+  var list = "<ul>";
+  for (let i = 0; i < filelist.listen; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list + "</ul>";
+  return list;
+}
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function (error, filelist) {
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`
+            );
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
