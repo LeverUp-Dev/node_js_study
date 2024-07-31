4eb4ff1e96e28624bb8b0765fe0eb29d99e2694b
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-31 21:51:59 +0900

    수정할 내용 전송

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -79,7 +79,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"/></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -109,6 +109,36 @@ var app = http.createServer(function (request, response) {
         function (err) {
           response.writeHead(302, { location: `/?id=${title}` });
           response.end("seccess");
+        }
+      );
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./web2_nodejs/data", function (error, filelist) {
+      fs.readFile(
+        `web2_nodejs/data/${queryData.id}`,
+        "utf8",
+        function (err, description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value"${title}">
+              <p><input type="text" name="title" placeholder="title" value=${title} /></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
         }
       );
     });
